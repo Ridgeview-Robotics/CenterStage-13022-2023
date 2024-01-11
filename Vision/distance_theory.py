1b4ed906454160107d35be79cc77db86168e3a76
Salty <74743320+Bismarck-Override@users.noreply.github.com>
2024-01-10 22:26:40 -0700

    attempt at shape recognition

--- Vision/distance_theory.py
+++ Vision/distance_theory.py
@@ -8,3 +8,33 @@ realHeight = 80.156
 sensorHeight = 2.02
 
 irlHeight = (focalPoint * realHeight * imageHeight)/(objectHeight * sensorHeight)
+
+img = cv2.imread('shapes.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret,thresh = cv2.threshold(gray,127,255,1)
+
+contours,h = cv2.findContours(thresh,1,2)
+
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+    print len(approx)
+    if len(approx)==5:
+        print "pentagon"
+        cv2.drawContours(img,[cnt],0,255,-1)
+    elif len(approx)==3:
+        print "triangle"
+        cv2.drawContours(img,[cnt],0,(0,255,0),-1)
+    elif len(approx)==4:
+        print "square"
+        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+    elif len(approx) == 9:
+        print "half-circle"
+        cv2.drawContours(img,[cnt],0,(255,255,0),-1)
+    elif len(approx) > 15:
+        print "circle"
+        cv2.drawContours(img,[cnt],0,(0,255,255),-1)
+
+cv2.imshow('img',img)
+cap.release()
+cv2.destroyAllWindows()
